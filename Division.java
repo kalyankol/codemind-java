ae8d52781644f284936f44123d5a043b8a18a42c
kalyan <103946030+kalyankol@users.noreply.github.com>
2022-08-30 15:08:28 +0530

    import java.util.Scanner; class HelloWorld {     public static void main(String[] args) {       Scanner scanner=new Scanner(System.in);       int a=scanner.nextInt();       int b=scanner.nextInt();       int c=a/b;       System.out.print(c);     } }

---
+++ Division.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class HelloWorld {
+    public static void main(String[] args) {
+      Scanner scanner=new Scanner(System.in);
+      int a=scanner.nextInt();
+      int b=scanner.nextInt();
+      int c=a/b;
+      System.out.print(c);
+    }
+}
