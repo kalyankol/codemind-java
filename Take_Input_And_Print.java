def43ff767a4729b8686799e6045895d9e352fdc
kalyan <103946030+kalyankol@users.noreply.github.com>
2022-08-30 15:11:56 +0530

    import java.util.Scanner; class HelloWorld {     public static void main(String[] args) {       Scanner scanner=new Scanner(System.in);       String input=scanner.nextLine();       System.out.print(input);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class HelloWorld {
+    public static void main(String[] args) {
+      Scanner scanner=new Scanner(System.in);
+      String input=scanner.nextLine();
+      System.out.print(input);
+    }
+}
