05d230ddea9b2f919932a163cbbe57c6ee17cf93
kalyan <103946030+kalyankol@users.noreply.github.com>
2022-09-04 14:24:34 +0530

    import java.util.Scanner; class HelloWorld {     public static void main(String[] args) {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] a=new int[n];         for(int i=0;i<n;i++){             a[i]=sc.nextInt();         }         int st=sc.nextInt();         int sp=sc.nextInt();         int c=0;         for(int i=0;i<n;i++){             if(a[i]>=st && a[i]<=sp){                 c+=a[i];             }         }         System.out.print(c);     } }

---
+++ sum_of_elements_between_A_and_B.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        int st=sc.nextInt();
+        int sp=sc.nextInt();
+        int c=0;
+        for(int i=0;i<n;i++){
+            if(a[i]>=st && a[i]<=sp){
+                c+=a[i];
+            }
+        }
+        System.out.print(c);
+    }
+}
