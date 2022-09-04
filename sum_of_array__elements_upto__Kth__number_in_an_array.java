ce6f96a160526b370c83259b14125af6b6a2b7b5
kalyan <103946030+kalyankol@users.noreply.github.com>
2022-09-04 14:57:14 +0530

    import java.util.Scanner; class HelloWorld {     public static void main(String[] args) {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] a=new int[n];         for(int i=0;i<n;i++){             a[i]=sc.nextInt();         }         int k=sc.nextInt();         int c=0;         for(int i=0;i<n;i++){             if(i<k){                 c+=a[i];             }         }         System.out.print(c);     } }

---
+++ sum_of_array__elements_upto__Kth__number_in_an_array.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        int k=sc.nextInt();
+        int c=0;
+        for(int i=0;i<n;i++){
+            if(i<k){
+                c+=a[i];
+            }
+        }
+        System.out.print(c);
+    }
+}
