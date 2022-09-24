fe1bf862afbb2dfc724bb2d5d4f43f68b1936dee
kalyan <103946030+kalyankol@users.noreply.github.com>
2022-09-24 15:15:20 +0530

    import java.util.*; class ascii{     public static void main(String agrs[]){         Scanner sc=new Scanner(System.in);         char ch=sc.nextLine().charAt(0);         int asci=ch;         System.out.print(asci);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class ascii{
+    public static void main(String agrs[]){
+        Scanner sc=new Scanner(System.in);
+        char ch=sc.nextLine().charAt(0);
+        int asci=ch;
+        System.out.print(asci);
+    } 
+}
