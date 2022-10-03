ae1507acb19c394d177c731d7e77f805261756e5
kalyan <103946030+kalyankol@users.noreply.github.com>
2022-10-04 00:29:49 +0530

    import java.util.*; class palin{     public static boolean pal(int n){         boolean p=false;         int a=n,r,rem=0;         while(n!=0){             r=n%10;             rem=(rem*10)+r;             n=n/10;         }         if(a==rem){             p=true;         }         return p;     }     public static void main(String agrs[]){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] a=new int[n];         for(int i=0;i<n;i++){             a[i]=sc.nextInt();         }         int c=0;         for(int i=0;i<n;i++){             if(pal(a[i])){                 c+=1;             }         }         System.out.print(c);     } }

--- Count_palindromes.java
+++ Count_palindromes.java
@@ -1,32 +1,28 @@
-import java.util.Scanner;
-class HelloWorld {
-    static boolean pal(int n){
-        int a=n;
-        int rem=0;
-        int r;
-        while(n>0){
+import java.util.*;
+class palin{
+    public static boolean pal(int n){
+        boolean p=false;
+        int a=n,r,rem=0;
+        while(n!=0){
             r=n%10;
             rem=(rem*10)+r;
             n=n/10;
         }
-        if(rem==a){
-            return true;
+        if(a==rem){
+            p=true;
         }
-        else{
-            return false;
-        }
-    } 
-    public static void main(String[] args) {
-        Scanner scan=new Scanner(System.in);
-        int n=scan.nextInt();
-        int[] arr=new int[n];
+        return p;
+    }
+    public static void main(String agrs[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
         for(int i=0;i<n;i++){
-           arr[i]=scan.nextInt(); 
+            a[i]=sc.nextInt();
         }
         int c=0;
         for(int i=0;i<n;i++){
-            boolean ispal=pal(arr[i]);
-            if(ispal){
+            if(pal(a[i])){
                 c+=1;
             }
         }
