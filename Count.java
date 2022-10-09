d696046683518ed8603bd2f9fb5341d7140c44ef
kalyan <103946030+kalyankol@users.noreply.github.com>
2022-10-10 00:17:22 +0530

    import java.util.*; class eoo{     public static void main(String agrs[]){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] a=new int[n];         for(int i=0;i<n;i++){             a[i]=sc.nextInt();         }         int e=0,o=0;         for(int i=0;i<n;i++){             if(a[i]%2==0){                 e+=1;             }             else{                 o+=1;             }         }         System.out.print(e+" "+o);     } }

---
+++ Count.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class eoo{
+    public static void main(String agrs[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        int e=0,o=0;
+        for(int i=0;i<n;i++){
+            if(a[i]%2==0){
+                e+=1;
+            }
+            else{
+                o+=1;
+            }
+        }
+        System.out.print(e+" "+o);
+    }
+}
