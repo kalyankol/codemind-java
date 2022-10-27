f7d3cec373d37d32047b4ddc89599b63760816a6
kalyan <103946030+kalyankol@users.noreply.github.com>
2022-10-27 10:10:35 +0530

    import java.util.*; class str{     public static void main(String agrs[]){         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         System.out.print(s.toLowerCase());     } }

---
+++ To_Lower_Case.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class str{
+    public static void main(String agrs[]){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.print(s.toLowerCase());
+    }
+}
